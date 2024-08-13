04510173cb2484d535ced07ad194003cfefa4093
Hevelin de Jesus Freitas <hevelinfreitas21@gmail.com>
2024-08-12 21:57:28 -0300

    :sparkles: feat: Created component login and register;

---
+++ src/components/LoginRegister.vue
@@ -0,0 +1,51 @@
+<template>
+  <q-form @submit="submitForm">
+    <q-input v-if="tab == 'register'" class="q-mb-md" outlined v-model="formData.name" label="Name"></q-input>
+    <q-input class="q-mb-md" outlined type="email" v-model="formData.email" label="E-mail"></q-input>
+    <q-input class="q-mb-md" outlined type="password" v-model="formData.password" label="Password"></q-input>
+    <div class="row">
+      <q-space/>
+      <q-btn type="submit" color="primary" :label="tab"></q-btn>
+    </div>
+
+
+  </q-form>
+</template>
+
+<script setup>
+defineOptions({
+  name: 'LoginRegister',
+  data(){
+    return {
+      formData:{
+        name: '',
+        email: '',
+        password: ''
+      }
+    }
+  },
+  methods:{
+    submitForm(){
+      if(this.tab == login){
+        console.log('login user')
+      }else{
+        console.log('register user')
+      }
+    }
+  }
+})
+
+const props = defineProps({
+  tab:{
+    required: true
+  },
+
+})
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
+
--- src/pages/PageAuth.vue
+++ src/pages/PageAuth.vue
@@ -1,12 +1,47 @@
 <template>
-  <q-page class="flex">
-    <h5>Auth Page</h5>
+  <q-page class="flex q-pa-md">
+    <q-card class="full-width">
+      <q-tabs
+        v-model="tab"
+        dense
+        class="text-grey"
+        active-color="primary"
+        indicator-color="primary"
+        align="justify"
+        narrow-indicator
+      >
+        <q-tab name="login" label="Login" />
+        <q-tab name="register" label="Register" />
+      </q-tabs>
 
+      <q-separator />
+
+      <q-tab-panels v-model="tab" animated>
+        <q-tab-panel name="login">
+          <login-register :tab="tab"></login-register>
+        </q-tab-panel>
+
+        <q-tab-panel name="register">
+          <login-register :tab="tab"></login-register>
+        </q-tab-panel>
+      </q-tab-panels>
+    </q-card>
   </q-page>
 </template>
 
 <script setup>
-defineOptions({
+import { defineAsyncComponent, ref } from "vue";
 
+defineOptions({
+  data() {
+    return {
+      tab: "login",
+    };
+  },
+  components: {
+    LoginRegister: defineAsyncComponent(() =>
+      import("components/LoginRegister.vue")
+    ),
+  },
 });
 </script>
