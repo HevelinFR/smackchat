41b9afc10a6f56974687f82c3643a76bfc3dcc2c
Hevelin de Jesus Freitas <hevelinfreitas21@gmail.com>
2024-08-20 08:11:07 -0300

    :lock:

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -5,12 +5,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBnRxdKWkd0ha4ntnAoBB_gY0LZhltnC6Q",
-  authDomain: "smackchat-85c49.firebaseapp.com",
-  projectId: "smackchat-85c49",
-  storageBucket: "smackchat-85c49.appspot.com",
-  messagingSenderId: "893473111011",
-  appId: "1:893473111011:web:62fd311831bea638c33ed6"
+ 
 };
 
 // Initialize Firebase
